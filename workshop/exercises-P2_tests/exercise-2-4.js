f0acb6fbed133b8101a9ba34ed5a232db00df771
Robbie Bart <adm@AdminisatorsAir.lan>
2020-06-22 16:43:23 -0400

    q2.4 done

--- workshop/exercises-P2_tests/exercise-2-4.js
+++ workshop/exercises-P2_tests/exercise-2-4.js
@@ -9,14 +9,32 @@
 // getLetterAtIndex("", 4);      // undefined
 // getLetterAtIndex("abc", 0);   // a
 
+let name = 'Rob' //['R','o','b']
+// console.log(name[0])
+// console.log(name.charAt(0))
+
+console.log(typeof 5)
+console.log(typeof 'hello')
+console.log(typeof {name:'ss'})
+
 function getLetterAtIndex(str, index) {
-  // Your code here
+  if (typeof str !== 'string' || str === "") {
+    return undefined;
+  } else {
+    return `${str.charAt(index)}`;
+  }
 }
 
 // Add 6 more test cases
 expect(getLetterAtIndex('hello', 4), 'o');
+// console.log(getLetterAtIndex('hello', 4));
 expect(getLetterAtIndex('goodbye', 0), 'g');
-
+expect(getLetterAtIndex('2asdf', 0), '2');
+expect(getLetterAtIndex('Dog', 0), 'D');
+expect(getLetterAtIndex('patrick', 2), 't');
+expect(getLetterAtIndex(1, 0), undefined);
+expect(getLetterAtIndex('cat', 0), 'c');
+expect(getLetterAtIndex(undefined, 0), undefined);
 /**
  * -------------------------------------------------------------------
  * ⚠️ No changes necessary below. ⚠️
